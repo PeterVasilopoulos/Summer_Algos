1efcbbac5a7a1a3b6b5e38ddbb38509766e690b9
Peter Vasilopoulos <petervas@vt.edu>
2023-07-06 14:24:31 -0400

    Insert function created

--- July6.js
+++ July6.js
@@ -23,13 +23,51 @@ class BST {
         this.root = null
     }
 
+    // Insert Node Function
+    insert(data) {
+        // create a new node 
+        const newNode = new Node(data)
 
+        // create a runner variable
+        let runner = this.head
+        // create a runner to hold previous node
+        let previous = null
+
+        // run through loop to find out where to put node
+        while(runner) {
+            // set previous to current runner
+            previous = runner
+
+            // if new data is smaller than current, move runner left
+            if(newNode.data < runner.data) {
+                runner = runner.left
+            }
+            // if new data is larger than current, move runner right
+            if(newNode.data > runner.data) {
+                runner = runner.right
+            }
+        }
+
+        // set the left or right of previous node to the new node
+
+        // if value is smaller than current, set left
+        if(newNode.data < previous.data) {
+            previous.left = newNode
+        }
+
+        // if value is larger than current, set right
+        if(newNode.data > previous.data) {
+            previous.right = newNode
+        }
+    }
 
     // Given an integer array nums where 
     // the elements are sorted in ascending 
     // order, convert it to a height-balanced 
     // binary search tree
+    sortedArrayToBTS(nums) {
 
+    }
 }
 
 // ------------------------------
