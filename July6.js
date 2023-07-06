3016d3d7dc429341d02893e27999ee103f2e0f0b
Peter Vasilopoulos <petervas@vt.edu>
2023-07-06 14:13:27 -0400

    Created base code for file

--- July6.js
+++ July6.js
@@ -1,7 +1,4 @@
-// Given an integer array nums where 
-// the elements are sorted in ascending 
-// order, convert it to a height-balanced 
-// binary search tree.
+// BINARY SEARCH TREE ALGORITHMS
 
 // Node
 function TreeNode(val, left, right) {
@@ -9,3 +6,32 @@ function TreeNode(val, left, right) {
     this.left = (left === undefined ? null : left)
     this.right = (right === undefined ? null : right)
 }
+
+// Node Class
+class Node {
+    constructor(data) {
+        this.data = data
+
+        this.left = null
+        this.right = null
+    }
+}
+
+// BST Class
+class BST {
+    constructor() {
+        this.root = null
+    }
+
+
+
+    // Given an integer array nums where 
+    // the elements are sorted in ascending 
+    // order, convert it to a height-balanced 
+    // binary search tree
+
+}
+
+// ------------------------------
+// TESTING
+// ------------------------------
